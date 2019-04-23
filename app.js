b3fdd14f2ba448b7339552ffa07aedaf8c99e9b9
Arin Trongsantipong <arintrongs@gmail.com>
2019-04-23 12:45:30 +0700

    separate /calendar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const axios = require('axios')
-const moment = require('moment')
+
 const { sign, verify } = require('./jwt')
 
 const UserModel = require('./user')
@@ -11,7 +11,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static('public'))
 app.get('/', async (req, res) => {
   return res.send('Hiiiiiiii')
 })
@@ -154,41 +153,6 @@ app.post('/auth/update', async (req, res) => {
       { new: true, upsert: true }
     ).lean()
     return res.status(200).send({ selected_courses: user.selected_courses, last_update: user.last_update })
-  } catch (e) {
-    console.log(e.message)
-    return res.status(400).send({ result: 'failed' })
-  }
-})
-app.get('/calendar/:uid', async (req, res) => {
-  const uid = req.params.uid
-  const user = await UserModel.findById(uid).lean()
-  const access_token = user.access_token
-  const token_type = user.token_type
-  try {
-    let calendar = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:https://mycoursevillecal.firebasehosting.com\n'
-    const selected_courses = user.selected_courses
-    const now = moment()
-    for (var course of selected_courses) {
-      const { data } = await axios.get(
-        `https://mycourseville.com/api/v1/public/get/course/assignments?cv_cid=${course.cv_cid}&detail=1`,
-        {
-          headers: { Authorization: `${token_type} ${access_token}` }
-        }
-      )
-      const assigments = data.data
-      for (var assignment of assigments) {
-        const due_date_timestamp = moment(assignment.duetime * 1000)
-        const start_date = `${due_date_timestamp.format('YYYYMMDD')}T000000`
-        const due_date = `${due_date_timestamp.format('YYYYMMDD')}T${due_date_timestamp.format('HHmmss')}`
-        calendar += `BEGIN:VEVENT\n`
-        calendar += `DTSTAMP:${now.format('YYYYMMDD')}T${now.format(
-          'HHmmss'
-        )}\nDTSTART:${start_date}\nDTEND:${due_date}\nSUMMARY:[${course.course_no}] : ${assignment.title}\n`
-        calendar += `BEGIN:VALARM\nTRIGGER:-PT24H\nDESCRIPTION:${assignment.title}\nACTION:DISPLAY\nEND:VALARM\n`
-        calendar += `END:VEVENT\n`
-      }
-    }
-    return res.status(200).send(calendar + 'END:VCALENDAR')
   } catch (e) {
     console.log(e.message)
     return res.status(400).send({ result: 'failed' })
--- server.js
+++ server.js
@@ -2,13 +2,19 @@ require('dotenv').config()
 require('./db')
 const port = process.env.PORT || 3003
 const app = require('./app')
+const calendar = require('./calendar')
 const fs = require('fs')
 const https = require('https')
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(port, function () {
-  console.log(`app is listening to port ${port} on https`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    },
+    app
+  )
+  .listen(port, function() {
+    console.log(`app is listening to port ${port} on https`)
+  })
+calendar.listen(80)
