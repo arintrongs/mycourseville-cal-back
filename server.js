d6d98c3db14644a9aaaf453436c7fdfebb15f295
Arin Trongsantipong <l3lackclevil@hotmail.com>
2019-04-23 05:29:37 +0000

    fix for production

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 require('./db')
 const port = process.env.PORT || 3003
 const app = require('./app')
+const fs = require('fs')
+const https = require('https')
 
-app.listen(port, () => {
-  console.log('Express server listening on port ' + port)
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(port, function () {
+  console.log(`app is listening to port ${port} on https`)
 })
